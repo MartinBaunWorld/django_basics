cc5145e36228a0e8b15def0269c97674ad0d6aec
user <m@pinehq.com>
2019-07-24 20:27:47 +0300

    Manifest updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django_basics',
-    version='0.1.4',
+    version='0.1.5',
     description=(
         'Django functionality that I always end up needing'
     ),
