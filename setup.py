dde3e603dac9dde929e3c02e6a645366a6bf9667
user <m@pinehq.com>
2019-07-24 20:29:52 +0300

    Updated Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django_basics',
-    version='0.1.5',
+    version='0.1.6',
     description=(
         'Django functionality that I always end up needing'
     ),
